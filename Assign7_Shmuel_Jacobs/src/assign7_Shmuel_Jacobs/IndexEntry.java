054969adc120765a5e330482e06393c927b532dc
Shmuel Jacobs <shmuel.jacobs@temple.edu>
2017-04-19 13:47:52 -0400

    Created Index Entry Object to be stored in Binary Tree

---
+++ Assign7_Shmuel_Jacobs/src/assign7_Shmuel_Jacobs/IndexEntry.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign7_Shmuel_Jacobs;
+
+/**
+ *
+ * @author J
+ */
+public class IndexEntry {
+    
+}
