2b8df63968b9154b27a9fdb1a12401d26493927f
Shmuel Jacobs <shmuel.jacobs@temple.edu>
2017-04-21 18:30:10 -0400

    Created add listing methods

--- Assign7_Shmuel_Jacobs/src/assign7_Shmuel_Jacobs/IndexTree.java
+++ Assign7_Shmuel_Jacobs/src/assign7_Shmuel_Jacobs/IndexTree.java
@@ -19,55 +19,103 @@ import java.util.function.Function;
  * @author Shmuel Jacobs
  */
 public class IndexTree {
-    
+
     protected IndexEntryNode root;
-    
+
     //TODO: This is dummy method. Find a place to add it really, and decide bool
-    public boolean addListing(String word,
-            int numOccurrences, Collection<Integer> places){
-        root = new IndexEntryNode(word.toLowerCase(), places);
-        return true;
+    public boolean addListing(String word, Collection<Integer> places) {
+        IndexEntryNode insertion = new IndexEntryNode(word.toLowerCase(), places);
+        if (root == null) {
+            root = insertion;
+            return true;
+        } else {
+            return addListing(root, insertion);
+        }
     }
-    
-    //TODO: Dummy method. Recursively print all elements. Create recursive and wrap
+
+    /**
+     * Recursive method for adding new listings. Do not pass in a null root.
+     * Check for that in public.
+     *
+     * @param localRoot
+     * @param insertion
+     * @return true if listing was added
+     */
+    private boolean addListing(IndexEntryNode localRoot,
+            IndexEntryNode insertion) {
+        boolean added = false;
+        //case: insertion is less than localRoot
+        if (localRoot.compareTo(insertion) > 0) {
+            if (localRoot.left == null) {
+                localRoot.left = insertion;
+                added = true;
+            } else {
+                added = addListing(localRoot.left, insertion);
+            }
+        } //case: insertion is greater than localRoot
+        else if (localRoot.compareTo(insertion) < 0) {
+            if (localRoot.right == null) {
+                localRoot.right = insertion;
+                added = true;
+            } else {
+                added = addListing(localRoot.right, insertion);
+            }
+        }
+        //flow shouldn't reach here except when listings match
+        return added;
+    }
+
+    /**
+     * Print out entire index listing. Calls in0order traversal helper method.
+     *
+     * @return String of entire index.
+     */
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder inOrderSB = new StringBuilder();
         inOrderTraversalHelper(root, new NodePrinter(), inOrderSB);
-        
+
         return inOrderSB.toString();
     }
-    
+
     /*
     private String toString(IndexEntryNode localRoot){
         
         if(localRoot)
     } */
-    
     /**
      * Private helper method for methods that require inOrderTraversal.
+     *
      * @param localRoot root of the tree to traverse
      * @param func functional object implementing method workOnNode, the method
      * to call at each node
      * @param output StringBuilder or other object capable of carrying output
      */
     private void inOrderTraversalHelper(IndexEntryNode localRoot,
-            NodeWorker func, Object output){
-        if(localRoot != null){
+            NodeWorker func, StringBuilder output) {
+        if (localRoot != null) {
             inOrderTraversalHelper(localRoot.left, func, output);
             func.workOnNode(localRoot, output);
             inOrderTraversalHelper(localRoot.right, func, output);
+
         }
     }
 }
 
+/**
+ * Implementation of NodeWorker interface, to be used to print the node it's
+ * given.
+ *
+ * @author Shmuel Jacobs
+ */
 class NodePrinter implements NodeWorker {
+
     @Override
-    public void workOnNode(IndexEntryNode node, StringBuilder output){
+    public void workOnNode(IndexEntryNode node, StringBuilder output) {
         output.append(node.getWord());
-        for(int i = 0;
+        for (int i = 0;
                 i < node.numberListings();
-                i++){
+                i++) {
             output.append(i + "  ");
         }
         output.append('\n');
